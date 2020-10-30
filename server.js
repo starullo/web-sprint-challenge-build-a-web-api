5ccec2b827e064eefc0dde20f4cc75991fc5bc10
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-10-30 12:54:42 -0500

    added a way to get just all actions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const projectRouter = require('./projectrouter')
 const actionRouter = require('./actionrouter');
+const Actions = require('./data/helpers/actionModel')
 
 
 const server = express();
@@ -13,5 +14,16 @@ server.use('/api/projects', actionRouter);
 server.get('/', (req, res)=>{
     res.send(`LET'S DO THIS THANGGG YAY`);
 });
+
+server.get('/actions', (req, res)=>{
+    Actions.get()
+    .then(data=>{
+        res.status(200).json(data)
+    })
+    .catch(err=>{
+        res.status(500).json({message: 'Something went wrong, sorry'})
+    })
+    
+})
 
 module.exports = server;
